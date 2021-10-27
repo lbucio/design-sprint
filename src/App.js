2a7a84388c1f6802cf7b36bf1e3ec89c209e2683
Larry Bucio <l.bucio@aggiemail.usu.edu>
2021-10-26 19:03:55 -0600

    Setup github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card/card';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div className="container">
+        <h1>Lucid Suite</h1>
+        <h2>Checkout what our products can do together</h2>
+
+        <div className="card-container">
+          <Card>
+            Hello
+          </Card>
+          <Card>
+            Hello
+          </Card>
+        </div>
+      </div>
+
     </div>
   );
 }
